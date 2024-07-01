0c6a9c83acafa3e1c3f8b60f4819c45437a2c830
wyattscarpenter <wyattscarpenter@gmail.com>
2024-07-01 09:33:17 -0400

    now make it optional
    Signed-off-by: wyattscarpenter <wyattscarpenter@gmail.com>

--- src/databricks/sql/utils.py
+++ src/databricks/sql/utils.py
@@ -134,7 +134,7 @@ class CloudFetchQueue(ResultSetQueue):
         schema_bytes,
         max_download_threads: int,
         start_row_offset: int = 0,
-        result_links: List[TSparkArrowResultLink] = None,
+        result_links: Optional[List[TSparkArrowResultLink]] = None,
         lz4_compressed: bool = True,
         description: List[List[Any]] = None,
     ):
