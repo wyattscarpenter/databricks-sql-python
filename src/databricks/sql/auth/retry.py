2ece43258a042de802fe705b1248d339197257e7
wyattscarpenter <wyattscarpenter@gmail.com>
2024-07-01 03:22:39 -0400

    Return type 'Retry' of 'new' incompatible with return type 'DatabricksRetryPolicy' in supertype 'Retry'
    Signed-off-by: wyattscarpenter <wyattscarpenter@gmail.com>

--- src/databricks/sql/auth/retry.py
+++ src/databricks/sql/auth/retry.py
@@ -162,7 +162,7 @@ class DatabricksRetryPolicy(Retry):
         new_object.command_type = command_type
         return new_object
 
-    def new(self, **urllib3_incremented_counters: typing.Any) -> Retry:
+    def new(self, **urllib3_incremented_counters: typing.Any) -> DatabricksRetryPolicy:
         """This method is responsible for passing the entire Retry state to its next iteration.
 
         urllib3 calls Retry.new() between successive requests as part of its `.increment()` method
